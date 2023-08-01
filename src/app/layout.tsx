f24fad5a657bd51b4c27d524370fd6511e37e357
imnotmoon <imnotmoon@gmail.com>
2023-08-01 23:12:10 +0900

    Install classNames, scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import React from 'react';
+import styles from './layout.module.scss';
+import classNames from 'classnames';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={classNames(inter.className, styles.root)}>{children}</body>
     </html>
   );
 }
