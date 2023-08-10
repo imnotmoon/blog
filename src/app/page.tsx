b6af16ed53c042a9f34ab9c261a629f0faca3dff
imnotmoon <imnotmoon@gmail.com>
2023-08-10 23:33:13 +0900

    Add styles to MainPost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import { MainPost } from '@/components/post/main/MainPost';
 import { post } from '@/components/post/Post.mock';
 import { Section } from '@/components/section/Section';
 import { Container } from '@/components/container/Container';
+import Link from 'next/link';
 
 const Home = () => (
   <main>
     <Section>
       <Container>
-        <MainPost {...post} />
+        <Link href={`/blog/post/${post.id}`}>
+          <MainPost {...post} />
+        </Link>
       </Container>
     </Section>
     <Section>
